697a088e599687fc94e3b6ef961d90733b0a106a
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-27 22:04:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     binding.pry 
-    array = self.split(".")
+    array = self.split(//)
     array2 = self.split("?")
     array3 = self.split("!")
     (array+array2+array3).count 
